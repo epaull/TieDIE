1236f793ed6bbf0954f654150532b3bbb2617a8a
Evan Paull <evanpaull@gmail.com>
2013-06-12 16:38:20 -0700

    --rename

--- test/run-all.sh
+++ test/run-all.sh
@@ -3,7 +3,7 @@
 echo "Running Kernel Tests..."
 ./kernel_tests.py
 echo "Running General Utility Functional Tests..."
-./util_test.py
+./util_tests.py
 
 echo "Running Regression Tests..."
 ./regression.py
