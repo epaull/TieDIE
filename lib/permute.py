42b9b310ffdc796fcd9f60ad317abf58782889d3
epaull <evanpaull@gmail.com>
2013-07-08 12:11:06 -0700

    --fix tabspace

--- lib/permute.py
+++ lib/permute.py
@@ -5,135 +5,135 @@ from scipy.stats import norm
 from math import log
 
 class NetBalancedPermuter:
-	"""
+    """
 
-		Encapsulates the permutation logic for an input heat set. Permutes
-		Node scores with other nodes of similar network degree by sorting
-		all nodes by degree, and binning them into blocks of a set size. 
-		Permutations are done only within blocks, so that the degree distribution
-		of input nodes is preserved.
-	"""
+        Encapsulates the permutation logic for an input heat set. Permutes
+        Node scores with other nodes of similar network degree by sorting
+        all nodes by degree, and binning them into blocks of a set size. 
+        Permutations are done only within blocks, so that the degree distribution
+        of input nodes is preserved.
+    """
 
 
-	def __init__(self, network, up_set):
-		"""
-			Input:
-				network: net[source] = [(i, t)]
-				up_set: up_set[node] = score
-		"""
-	
+    def __init__(self, network, up_set):
+        """
+            Input:
+                network: net[source] = [(i, t)]
+                up_set: up_set[node] = score
+        """
+    
 
-		# store node-degrees for all in network
-		self.degrees = {}
+        # store node-degrees for all in network
+        self.degrees = {}
 
-		# the set of initial nodes to permute within blocks: save it to the 
-		# instantiated object here
-		self.nodes = up_set.keys()
-		# heuristic: block needs to be significantly larger than the input set size
-		BLOCK_MULTIPLE = 10
-		self.block_size = len(self.nodes)*BLOCK_MULTIPLE
-		self.scores = {}
-		for node in self.nodes:
-			# save the scores as a set of tuples 
-			self.scores[(node, str(up_set[node]))] = 1
+        # the set of initial nodes to permute within blocks: save it to the 
+        # instantiated object here
+        self.nodes = up_set.keys()
+        # heuristic: block needs to be significantly larger than the input set size
+        BLOCK_MULTIPLE = 10
+        self.block_size = len(self.nodes)*BLOCK_MULTIPLE
+        self.scores = {}
+        for node in self.nodes:
+            # save the scores as a set of tuples 
+            self.scores[(node, str(up_set[node]))] = 1
 
-		# Compute total degree for each node in the network
-		for source in network:
+        # Compute total degree for each node in the network
+        for source in network:
 
-			if source not in self.degrees:
-				self.degrees[source] = 0
+            if source not in self.degrees:
+                self.degrees[source] = 0
 
-			for (i, target) in network[source]:	
-				self.degrees[source] += 1
+            for (i, target) in network[source]:    
+                self.degrees[source] += 1
 
-				if target not in self.degrees:
-					self.degrees[target] = 0
-				# add a degree for the incoming edge
-				self.degrees[target] += 1	
+                if target not in self.degrees:
+                    self.degrees[target] = 0
+                # add a degree for the incoming edge
+                self.degrees[target] += 1    
 
-		# reverse-sort the degrees of all nodes in the network.
-		self.sorted_degrees = sorted(self.degrees.items(), key=lambda x:x[1], reverse=True)
+        # reverse-sort the degrees of all nodes in the network.
+        self.sorted_degrees = sorted(self.degrees.items(), key=lambda x:x[1], reverse=True)
 
-			
-	def permuteBlock(self, block):
-		"""
-		Take a block of nodes and randomly shuffle using python's random.shuffle method.  	
+            
+    def permuteBlock(self, block):
+        """
+        Take a block of nodes and randomly shuffle using python's random.shuffle method.      
 
-		Input:
+        Input:
 
-			An array of node labels
-		
-		Returns:
-		
-			A hash mapping the original nodes to the nodes to swap with each. 
-		"""
-		# make a copy
-		orig = copy(block)
-		b = copy(block)
-		map = {}
-		# shuffle the copy in-place with the random.shuffle() method.
-		shuffle(b)
-		for i in range(0, len(b)):
-			# build the mapping from original to new
-			map[orig[i]] = b[i]
+            An array of node labels
+        
+        Returns:
+        
+            A hash mapping the original nodes to the nodes to swap with each. 
+        """
+        # make a copy
+        orig = copy(block)
+        b = copy(block)
+        map = {}
+        # shuffle the copy in-place with the random.shuffle() method.
+        shuffle(b)
+        for i in range(0, len(b)):
+            # build the mapping from original to new
+            map[orig[i]] = b[i]
 
-		return map
+        return map
 
-	def permuteOne(self):
-		"""
-		Generate one permutation of scores for all nodes, and return a hash of { node : score }
-		for each. 
-		"""	
-		group_count = 0
-		permuted_scores = {}
-		# initialize a new block
-		block = []
-		for (node, degree) in self.sorted_degrees:
-			block.append(node)	
-			group_count += 1
-			# reset every time we use the block size
-			if group_count % self.block_size == 0:
-				# permute the order of this <block_size> block
-				map = self.permuteBlock(block)
-				for (node, score) in self.scores:
-					if node in map:
-						permuted_scores[map[node]] = float(score)
-				block = []	
+    def permuteOne(self):
+        """
+        Generate one permutation of scores for all nodes, and return a hash of { node : score }
+        for each. 
+        """    
+        group_count = 0
+        permuted_scores = {}
+        # initialize a new block
+        block = []
+        for (node, degree) in self.sorted_degrees:
+            block.append(node)    
+            group_count += 1
+            # reset every time we use the block size
+            if group_count % self.block_size == 0:
+                # permute the order of this <block_size> block
+                map = self.permuteBlock(block)
+                for (node, score) in self.scores:
+                    if node in map:
+                        permuted_scores[map[node]] = float(score)
+                block = []    
 
-		return permuted_scores	
+        return permuted_scores    
 
-	def permute(self, iterations):
-		"""
-		Generate an array of random permutations of node scores.
-		
-		Input:
-			iteration: the number of permutations to generate
+    def permute(self, iterations):
+        """
+        Generate an array of random permutations of node scores.
+        
+        Input:
+            iteration: the number of permutations to generate
 
-		Returns:
-			an array of hashes: each hash indexes the nodes to permuted scores
-		"""	
-		permuted = []	
-		for i in range(0, iterations):
-			permuted.append(self.permuteOne())
+        Returns:
+            an array of hashes: each hash indexes the nodes to permuted scores
+        """    
+        permuted = []    
+        for i in range(0, iterations):
+            permuted.append(self.permuteOne())
 
-		return permuted
+        return permuted
 
-	
-	@staticmethod
-	def fitLogNorm(vector, test_value):
-		"""
-			Fit a log-normal to the background distrubtion supplied. 
-			Get the p-value based on the log of the test value. 
+    
+    @staticmethod
+    def fitLogNorm(vector, test_value):
+        """
+            Fit a log-normal to the background distrubtion supplied. 
+            Get the p-value based on the log of the test value. 
 
-			Input:
-				vector: background distribution to fit
-				test_value: value to test against the fitted background distribution
+            Input:
+                vector: background distribution to fit
+                test_value: value to test against the fitted background distribution
 
-			Output:
-				A p-value based on that distribution	
-		"""
-		mean, sd = norm.fit([log(v) for v in vector])		
-		# just the cdf: this value should be smaller 
-		p_val = norm.cdf(log(test_value), loc=mean,scale=sd)		
+            Output:
+                A p-value based on that distribution    
+        """
+        mean, sd = norm.fit([log(v) for v in vector])        
+        # just the cdf: this value should be smaller 
+        p_val = norm.cdf(log(test_value), loc=mean,scale=sd)        
 
-		return p_val
+        return p_val
