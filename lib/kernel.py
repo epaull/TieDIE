0735cb21305fbd17e808c5c17970ccc6902f6b2a
epaull <evanpaull@gmail.com>
2013-07-08 11:27:06 -0700

    --add development code for symmetric background test, parametric test (not used by main program)

--- lib/kernel.py
+++ lib/kernel.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env  python2.7
 
 from numpy import genfromtxt, dot
+import numpy as np
 import sys
 import math
 
@@ -107,12 +108,53 @@ class Kernel:
             arry1.append(float(v1[key]))
             arry2.append(float(v2[key]))
 
-        mag_1 = math.sqrt(dot(arry1,arry1))
-        mag_2 = math.sqrt(dot(arry2,arry2))
+        norm_arry1 = [ a/sum(arry1) for a in arry1 ]
+        norm_arry2 = [ a/sum(arry2) for a in arry2 ]
 
-        cos_theta = dot(arry1,arry2)/(mag_1*mag_2)
+        mag_1 = math.sqrt(dot(norm_arry1,norm_arry1))
+        mag_2 = math.sqrt(dot(norm_arry2,norm_arry2))
+
+        cos_theta = dot(norm_arry1,norm_arry2)/(mag_1*mag_2)
 
         return math.acos(cos_theta)
+
+    @staticmethod
+    def getSYMKLDiv(v1, v2):
+		"""
+		<Development module> Get the symmetric Kullback-Leibler divergence between input vectors.
+		"""
+        return (Kernel.getKLDiv(v1, v2) + Kernel.getKLDiv(v2, v1) )/ 2
+
+    @staticmethod
+    def getKLDiv(v1, v2):
+        """
+		<Development module> Get the Kullback-Leibler divergence between input vectors, 
+		which typically represent diffused heat values. 
+		Input:
+			2 diffused heat vectors
+		Output:
+			(float) The KL-divergence metric between vectors
+        """
+
+		# convert values to ordered array
+        arry1 = []
+        arry2 = []
+        for key in v1:
+            arry1.append(float(v1[key]))
+            arry2.append(float(v2[key]))
+
+		# normalize both vectors, adding an epsilon (pseudo-count) value for zero-values
+        EPSILON = 0.00001
+        norm_arry1 = [ a+EPSILON/(sum(arry1)+len(arry1)*EPSILON) for a in arry1 ]
+        norm_arry2 = [ a+EPSILON/(sum(arry2)+len(arry2)*EPSILON) for a in arry2 ]
+
+		# calculate the KL-Div
+        div_sum = 0 
+        for i in range(0, len(norm_arry1)):
+            div_sum += np.log( (norm_arry1[i]/norm_arry2[i]) )*norm_arry1[i]
+
+        return div_sum
+
 
     def diffuse(self, vector, reverse=False):
         """
