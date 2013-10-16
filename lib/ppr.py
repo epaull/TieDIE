f6d6244192dadc4f5572c927d807bca64cb45557
Edward S. Rice <esrice@soe.ucsc.edu>
2013-10-15 21:06:29 -0700

    Documentation only.

--- lib/ppr.py
+++ lib/ppr.py
@@ -2,17 +2,21 @@
 
 import networkx as nx
 
+# TODO perhaps Diffuser could be abstracted to an interface for better code
+# clarity? - ESR
+# TODO should Diffusers take an nx DiGraph as initialization arguments rather
+# than create them? Storing graphss as hashes makes the code less clear IMO -ESR
 class PPrDiffuser:
 
 	def __init__(self, network):
-		'''
-			PPrDiffuser: object to perform the Personalized PageRank Algorithm
-			This method creates the diffuser object from an networkx DiGraph() object, 
-			which can then be used to diffuse vectors over this
-			
-			Input:
-				- network : a network hash object
-		'''
+		"""
+		PPrDiffuser: object to perform the Personalized PageRank Algorithm
+		This method creates the diffuser object from an networkx DiGraph() object, 
+		which can then be used to diffuse vectors over this
+		
+		Input:
+			- network : a network hash object
+		"""
 
 		# create a directed graph with NetworkX
 		self.G = nx.DiGraph()
@@ -25,18 +29,18 @@ class PPrDiffuser:
 				self.G_reversed.add_edge(t, source)
 
 	def personal_page_rank(self, p_vector, reverse=False):
-		'''
-			Personal_Page_Rank: Get the personal pagerank of the supplied input vector
+		"""
+		Personal_Page_Rank: Get the personal pagerank of the supplied input vector
 
-			Input: 
-				- p_vector: A hash-map of input values for a selection (or all) nodes
-				(if supplied nodes aren't in the graph, they will be ignored)
+		Input: 
+			- p_vector: A hash-map of input values for a selection (or all) nodes
+			(if supplied nodes aren't in the graph, they will be ignored)
 
-			Output:
-				- A vector of diffused heats in hash-map (key,value) format
-		'''
+        Output:
+			- A vector of diffused heats in hash-map (key,value) format
+		"""
 		input_pvec = None
-		#  without initializing this vector the initial probabilities will be flat
+		# without initializing this vector the initial probabilities will be flat
 		# and this will be equivalent to standard page rank
 		if p_vector:
 			input_pvec = {}
@@ -55,14 +59,21 @@ class PPrDiffuser:
 			return nx.pagerank_numpy(self.G, 0.85, input_pvec)
 
 	def diffuse(self, p_vector, reverse=False):
-		'''
-			Diffuse: perform generalized diffusion from the supplied input vector
+		"""
+		Diffuse: perform generalized diffusion from the supplied input vector
 
-			Input: 
-				- p_vector: A hash-map of input values for a selection (or all) nodes
-				(if supplied nodes aren't in the graph, they will be ignored)
+		Input: 
+			- p_vector: A dict of initial heat values for nodes. Keys are nodes'
+              ID strings. Any node not given in the dictionary will receive no
+              initial heat. Any supplied nodes not found in the graph will be
+              ignored.
 
-			Output:
-				- A vector of diffused heats in hash-map (key,value) format
-		'''
+        Optional args:
+            - reverse: reverse the direction of edges in the graph before
+              applying heat. default=False
+
+        Returns:
+			- A vector of diffused heats in dict format where keys are the
+              nodes' ID strings
+    	"""
 		return self.personal_page_rank(p_vector, reverse)
