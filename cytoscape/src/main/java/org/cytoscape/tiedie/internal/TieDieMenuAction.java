8bee98243fc43b4e9b75c3ab59771e84f001b935
srikanthBezawada <srikanth.5283@gmail.com>
2014-05-23 19:39:14 +0530

    creates a new menu item with name TieDIE under Apps menu section of cytoscape

---
+++ cytoscape/src/main/java/org/cytoscape/tiedie/internal/TieDieMenuAction.java
@@ -0,0 +1,31 @@
+package org.cytoscape.tiedie.internal;
+
+import java.awt.event.ActionEvent;
+import org.cytoscape.application.CyApplicationManager;
+import org.cytoscape.application.swing.AbstractCyAction;
+import org.cytoscape.application.swing.CySwingApplication;
+
+/**
+ *  Creates a new menu item under Apps menu section.
+ *  @author SrikanthB
+ */
+public class TieDieMenuAction extends AbstractCyAction{
+    
+    public CyApplicationManager cyApplicationManager;
+    public CySwingApplication cyDesktopService;
+    public CyActivator cyactivator;
+
+    public TieDieMenuAction(CyApplicationManager cyApplicationManager, final String menuTitle, CyActivator cyactivator) {
+        super(menuTitle, cyApplicationManager, null, null);
+        setPreferredMenu("Apps");
+        this.cyactivator = cyactivator;
+        this.cyApplicationManager = cyApplicationManager;
+        this.cyDesktopService = cyactivator.getcytoscapeDesktopService();
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        TieDieCore tiediecore = new TieDieCore(cyactivator);
+    }
+    
+    
+}
